707163abca13b3b5fe30cae6ea2530b560677594
Mingyu-Potato <mingyu6952@gmail.com>
2022-11-03 17:18:57 +0900

    1103

--- krri_alarm.py
+++ krri_alarm.py
@@ -2,30 +2,37 @@ from playsound import playsound
 import schedule
 import time
 import sys
+import multiprocessing
+
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import Audio
 
 def lunch():
     print('-----Music Start-----')
     print('-----11:25분은 점심시간-----')
-    playsound('sample.mp3')
+    playsound('lunch.mp3')
 
 def getoffwork():
     print('-----Music Start-----')
     print('-----17:55분은 퇴근시간-----')
-    playsound('sample.mp3')
+    playsound('lunch.mp3')
 
-def end_alarm():
-    print('-----Music End-----')
-    sys.exit(0)
+def _init():
+    schedule.every().day.at('11:25').do(lunch)
+    schedule.every().day.at('17:55').do(getoffwork)
 
-schedule.every().day.at('11:25').do(lunch)
-schedule.every().day.at('11:30').do(end_alarm)
-schedule.every().day.at('17:55').do(getoffwork)
-schedule.every().day.at('18:00').do(end_alarm)
+def run():
+    _init()
+    print('Schedule 실행 중...')
 
-while True:
-    try:
-        schedule.run_pending()
-        time.sleep(1)
-    except KeyboardInterrupt:
-        print('프로그램 종료')
-        sys.exit(0)
+    while True:
+        try:
+            schedule.run_pending()
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print('프로그램 종료')
+            sys.exit(0)
+
+if __name__ == '__main__':
+    run()
